848fced57c9c1538663e6b0d30545e399327b8da
Lawrence Buckingham <buckingl@qut.edu.au>
2022-08-05 11:35:05 +1000

    Add exercise 6.

---
+++ Exercises/06_LocateSymbol/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LocateSymbol {
+    public class Program {
+        const string PROMPT = "Please enter a line of text:";
+        const string POSITIONS = "The first and last positions of '{0}' are {1} and {2}, respectively.";
+        const char SYMBOL = '^';
+
+        public static void Main( string[] args ) {
+            // Keep the following line intact 
+            Console.WriteLine( "===========================" );
+
+            Console.WriteLine( PROMPT );
+            string searchText = Console.ReadLine();
+            
+            int firstPos = searchText.IndexOf(SYMBOL);
+            int lastPos = searchText.LastIndexOf(SYMBOL);
+
+            Console.WriteLine(POSITIONS, SYMBOL, firstPos, lastPos);
+
+            // Keep the following line intact 
+            Console.WriteLine( "===========================" );
+        }
+    }
+}
